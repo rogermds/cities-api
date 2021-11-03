ea53c19df760633d32fd30e5dff0340d74059ac6
Rogerio Martins <86385568+rogermds@users.noreply.github.com>
2021-11-03 19:09:23 -0300

    Adiciona paginação a API de países usando métodos do SpringBoot

--- src/main/java/com/github/rogermds/citiesapi/CountryResource.java
+++ src/main/java/com/github/rogermds/citiesapi/CountryResource.java
@@ -3,6 +3,8 @@ package com.github.rogermds.citiesapi;
 import com.github.rogermds.citiesapi.countries.Country;
 import com.github.rogermds.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,7 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
     }
 }
